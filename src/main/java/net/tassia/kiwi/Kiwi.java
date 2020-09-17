5c06b02ca83416c2e0857d1ca9c3607d073830cb
Tassilo <tassia710@gmail.com>
2020-09-18 00:57:40 +0200

    Added escape XSS

--- src/main/java/net/tassia/kiwi/Kiwi.java
+++ src/main/java/net/tassia/kiwi/Kiwi.java
@@ -184,4 +184,21 @@ public class Kiwi {
 	}
 	/* Driver Wrapper */
 
+
+
+
+
+	/* Escape XSS */
+	/**
+	 * Escapes a string to make it cross site scripting safe. This method is confirm with the
+	 * <a href="https://cheatsheetseries.owasp.org/cheatsheets/Cross_Site_Scripting_Prevention_Cheat_Sheet.html">
+	 *     OWASP Cross Site Scripting Prevention Cheat Sheet</a>.
+	 * @param input the string to escape
+	 * @return the escaped string
+	 */
+	public static String escapeXSS(String input) {
+		return EscapeXSSEngine.escapeXSS(input);
+	}
+	/* Escape XSS */
+
 }
