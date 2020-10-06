1a8905a3883209223379a57690c319c1de274bfc
Tassilo <tassia710@gmail.com>
2020-10-06 16:45:46 +0200

    Update HttpStatusCategory.java

--- src/main/java/net/tassia/kiwi/HttpStatusCategory.java
+++ src/main/java/net/tassia/kiwi/HttpStatusCategory.java
@@ -1,15 +1,33 @@
 package net.tassia.kiwi;
 
+/**
+ * TODO
+ */
 public enum HttpStatusCategory {
 
+	/**
+	 * TODO
+	 */
 	INFORMATIONAL,
 
+	/**
+	 * TODO
+	 */
 	SUCCESS,
 
+	/**
+	 * TODO
+	 */
 	REDIRECTION,
 
+	/**
+	 * TODO
+	 */
 	CLIENT_ERROR,
 
+	/**
+	 * TODO
+	 */
 	SERVER_ERROR
 
 }
