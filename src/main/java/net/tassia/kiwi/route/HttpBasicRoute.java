13f5a1ccf4915f0050fa23f2e3de4269ba6b9c02
Tassilo <tassia710@gmail.com>
2020-10-17 15:26:04 +0200

    Add JavaDoc

--- src/main/java/net/tassia/kiwi/route/HttpBasicRoute.java
+++ src/main/java/net/tassia/kiwi/route/HttpBasicRoute.java
@@ -4,11 +4,20 @@ import net.tassia.kiwi.HttpRequest;
 import net.tassia.kiwi.HttpResponse;
 import net.tassia.kiwi.Kiwi;
 
+/**
+ * This is the basic route. This is as simple as it can get, no data parsing or manipulating is happening here.
+ * The {@link #process(Kiwi, HttpRequest, HttpResponse, String[])} function is directly called from the driver.
+ */
 @FunctionalInterface
 public interface HttpBasicRoute {
 
 	/**
-	 * TODO
+	 * Processes this HTTP request.
+	 * @param kiwi main
+	 * @param request the request
+	 * @param response the response
+	 * @param matches the matches if the route regex specified any groups
+	 * @return the data to send back or <code>null</code> if data was set via {@link HttpResponse#setData(byte[])}
 	 */
 	byte[] process(Kiwi kiwi, HttpRequest request, HttpResponse response, String[] matches);
 
