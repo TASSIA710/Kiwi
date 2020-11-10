2cf6bbfd219a1631b9791ec74f8c87a0fedfbda8
Tassilo <tassia710@gmail.com>
2020-11-10 15:59:17 +0100

    Update MimeMessage.java

--- src/main/java/net/tassia/kiwi/MimeMessage.java
+++ src/main/java/net/tassia/kiwi/MimeMessage.java
@@ -1,6 +1,5 @@
 package net.tassia.kiwi;
 
-import java.io.InputStream;
 import java.util.Map;
 
 public abstract class MimeMessage {
@@ -8,10 +7,4 @@ public abstract class MimeMessage {
 	public abstract Map<String, String> getHeaders();
 	public abstract String getHeader(String key);
 	public abstract boolean hasHeader(String key);
-
-	public abstract boolean hasPayload();
-	public abstract int getPayloadLength();
-	public abstract InputStream getPayloadStream();
-	public abstract byte[] getPayload();
-
 }
