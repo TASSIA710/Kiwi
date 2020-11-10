dc3e99ada2790a8232286538ac30a425d53c40bd
Tassilo <tassia710@gmail.com>
2020-11-10 15:59:34 +0100

    Update Response.java

--- src/main/java/net/tassia/kiwi/Response.java
+++ src/main/java/net/tassia/kiwi/Response.java
@@ -3,9 +3,15 @@ package net.tassia.kiwi;
 import net.tassia.kiwi.enums.Status;
 import net.tassia.kiwi.enums.Version;
 
+import java.io.OutputStream;
+
 public abstract class Response extends MimeMessage {
 
 	public abstract Version getVersion();
 	public abstract Status getStatus();
+
+	public abstract OutputStream openPayloadStream();
+	public abstract void closePayloadStream();
+	public abstract void setPayload(byte[] payload);
 
 }
